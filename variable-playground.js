fe68d5038c0327c7a00491ec93a9bfd45d239a33
Tony Nguyen <dolongky@gmail.com>
2015-12-01 15:41:12 +0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+//var person = {
+//    name: 'Tony',
+//    age: 21
+//};
+//
+//function updatePerson(obj) {
+//    obj.age = 24;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+
+
+
+// Array examples
+var arr = [15, 20];
+
+function updateArr(array) {
+    array[1] = 30;
+    debugger;
+}
+
+updateArr(arr);
+console.log(arr);
