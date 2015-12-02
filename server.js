67950fe6553dc5883dd848d8b9d2e6acbee8def9
Tony Nguyen <dolongky@gmail.com>
2015-12-02 10:08:59 +0700

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -56,18 +57,24 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
+    db.todo.create(body).then(function (todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
 
-    body.description = body.description.trim();
-
-    body.id = todoNextId++;
-
-    todos.push(body);
-
-    res.json(body);
+    //    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //        return res.status(400).send();
+    //    }
+    //
+    //    body.description = body.description.trim();
+    //
+    //    body.id = todoNextId++;
+    //
+    //    todos.push(body);
+    //
+    //    res.json(body);
 });
 
 // DELETE /todos/:id
@@ -114,7 +121,8 @@ app.put('/todos/:id', function (req, res) {
     res.json(matchedTodo);
 });
 
-
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
